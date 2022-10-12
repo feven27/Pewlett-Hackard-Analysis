c6299a252ae2b3d4112ab76830bd082eb62bd3cd
feven27 <110137693+feven27@users.noreply.github.com>
2022-10-11 20:26:50 -0400

    File Analyzed

--- schema.sql
+++ schema.sql
@@ -1,14 +1,13 @@
 -- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
+CREATE TABLE Departments (
 	dept_no VARCHAR (4) NOT NULL,
 	dept_name VARCHAR (40) NOT NULL,
 	PRIMARY KEY (dept_no),
-    UNIQUE (dept_name)
-						 
+    UNIQUE (dept_name)						 
 );
 
-CREATE TABLE employees (
-	emp_no INT NOT NULL,
+CREATE TABLE Employees (
+	emp_no  NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
@@ -17,7 +16,7 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_manager (
+CREATE TABLE Dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
@@ -27,7 +26,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE salaries (
+CREATE TABLE Salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
   from_date DATE NOT NULL,
@@ -36,9 +35,9 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE Dep_Emp (
+CREATE TABLE Dept_Emp (
+  dept_no VARCHAR(4) NOT NULL,
   emp_no INT NOT NULL,
-  dept_no INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   PRIMARY KEY (emp_no)
@@ -52,4 +51,4 @@ CREATE TABLE Titles (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-SELECT * FROM departments;
+SELECT * FROM Departments;
